393da3f3f09068b06a8173a246287fe8bc0763b7
illia <smahliukillia927@gmail.com>
2026-01-31 23:16:10 +0100

    Add button to set grid size

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let generalDiv = document.querySelector("#general-div");
+let button = document.querySelector("#button");
+let block = document.createElement("div");
+block.setAttribute("id", "block");
 
-for (let i = 0; i < (960 * 960) / (16 * 16); i++) {
-  let block = document.createElement("div");
-  block.setAttribute("id", "block");
-  generalDiv.appendChild(block);
-}
+button.addEventListener("click", () => {
+  generalDiv.textContent = "";
+  number = prompt("введи число от 1 до 100");
+  let secondNumber = number * number;
+
+  for (let i = 0; i < secondNumber; i++) {
+    let block = document.createElement("div");
+    block.setAttribute("id", "block");
+    block.style.width = 960 / number + "px";
+    block.style.height = 960 / number + "px";
+
+    generalDiv.appendChild(block);
+  }
+});
 
 generalDiv.addEventListener("mousemove", (e) => {
   e.target.style.background = "blue";
